6c5862786be1fdc60230a2c314c92fb2cf1e0dff
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-10-06 18:11:56 +0200

    fix(wpa_supplicant): reconfigure network after changing config

--- src/wpa_supplicant/wpa_supplicant.rs
+++ src/wpa_supplicant/wpa_supplicant.rs
@@ -6,6 +6,7 @@ pub fn set_wifi(ssid: String, password: String) -> Result<()> {
     wpa.request(format!("SET_NETWORK 0 ssid \"{}\"", ssid).as_str())?;
     wpa.request(format!("SET_NETWORK 0 psk \"{}\"", password).as_str())?;
     wpa.request("SAVE_CONFIG")?;
+    wpa.request("RECONFIGURE")?;
 
     Ok(())
 }
