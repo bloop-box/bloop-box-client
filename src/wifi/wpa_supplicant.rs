75ea4453c492c4a47f5412cc02e312d14cd4036d
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2023-02-04 19:37:56 +0100

    fix(clippy): use format variables in-line

--- src/wifi/wpa_supplicant.rs
+++ src/wifi/wpa_supplicant.rs
@@ -3,8 +3,8 @@ use anyhow::Result;
 pub fn set_wifi(ssid: String, password: String) -> Result<()> {
     let mut wpa = wpactrl::Client::builder().open()?;
 
-    wpa.request(format!("SET_NETWORK 0 ssid \"{}\"", ssid).as_str())?;
-    wpa.request(format!("SET_NETWORK 0 psk \"{}\"", password).as_str())?;
+    wpa.request(format!("SET_NETWORK 0 ssid \"{ssid}\"").as_str())?;
+    wpa.request(format!("SET_NETWORK 0 psk \"{password}\"").as_str())?;
     wpa.request("SAVE_CONFIG")?;
     wpa.request("RECONFIGURE")?;
 
