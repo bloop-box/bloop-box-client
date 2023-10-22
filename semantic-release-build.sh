a77bfcde75bfaefdad4a1e5edef09551d3bcad93
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2023-10-22 22:51:22 +0200

    build: add aarch64 target for release

--- semantic-release-build.sh
+++ semantic-release-build.sh
@@ -4,28 +4,39 @@ VERSION="$1"
 
 cargo install cross cargo-deb
 sed -i '/\[package\]/,/^version = "[^"]*"$/ s/^version = "[^"]*"$/version = "'"$VERSION"'"/' Cargo.toml
+
 cross build --target arm-unknown-linux-gnueabihf --release || exit 1
+cross build --target aarch64-unknown-linux-gnu --release || exit 1
+
 cargo-deb -v --no-build --target arm-unknown-linux-gnueabihf --no-strip || exit 1
+cargo-deb -v --no-build --target aarch64-unknown-linux-gnu --no-strip || exit 1
 
-# This is workaround for https://github.com/kornelski/cargo-deb/issues/47
-# Patch the generated DEB to have ./ paths compatible with `unattended-upgrade`:
-pushd target/arm-unknown-linux-gnueabihf/debian || exit 1
-DEB_FILE_NAME=$(ls -1 *.deb | head -n 1)
-DATA_ARCHIVE=$(ar t "${DEB_FILE_NAME}"| grep -E '^data\.tar')
-ar x "${DEB_FILE_NAME}" "${DATA_ARCHIVE}"
-tar tf "${DATA_ARCHIVE}"
+fix_deb () {
+  target="$1"
 
-if [[ "${DATA_ARCHIVE}" == *.xz ]]; then
-  # Install XZ support that will be needed by TAR
-  apt-get -y install -y xz-utils
-  EXTRA_TAR_ARGS=J
-fi
+  # This is workaround for https://github.com/kornelski/cargo-deb/issues/47
+  # Patch the generated DEB to have ./ paths compatible with `unattended-upgrade`:
+  pushd "target/$target/debian" || exit 1
+  DEB_FILE_NAME=$(ls -1 *.deb | head -n 1)
+  DATA_ARCHIVE=$(ar t "${DEB_FILE_NAME}"| grep -E '^data\.tar')
+  ar x "${DEB_FILE_NAME}" "${DATA_ARCHIVE}"
+  tar tf "${DATA_ARCHIVE}"
 
-mkdir tar-hack
-tar -C tar-hack -xf "${DATA_ARCHIVE}"
-pushd tar-hack || exit 1
-tar c${EXTRA_TAR_ARGS}f "../${DATA_ARCHIVE}" --owner=0 --group=0 ./*
-popd || exit 1
-tar tf "${DATA_ARCHIVE}"
-ar r "${DEB_FILE_NAME}" "${DATA_ARCHIVE}"
-popd || exit 1
+  if [[ "${DATA_ARCHIVE}" == *.xz ]]; then
+    # Install XZ support that will be needed by TAR
+    apt-get -y install -y xz-utils
+    EXTRA_TAR_ARGS=J
+  fi
+
+  mkdir tar-hack
+  tar -C tar-hack -xf "${DATA_ARCHIVE}"
+  pushd tar-hack || exit 1
+  tar c${EXTRA_TAR_ARGS}f "../${DATA_ARCHIVE}" --owner=0 --group=0 ./*
+  popd || exit 1
+  tar tf "${DATA_ARCHIVE}"
+  ar r "${DEB_FILE_NAME}" "${DATA_ARCHIVE}"
+  popd || exit 1
+}
+
+fix_deb "arm-unknown-linux-gnueabihf"
+fix_deb "aarch64-unknown-linux-gnu"
